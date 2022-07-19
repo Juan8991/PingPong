2a1c76b473d347673e06558b16a1ca49d65fe66b
JuanR <ramirezjuan9806@gmail.com>
2022-07-18 20:45:26 -0500

    Creacion de las barras del pingpong mediante la funcion draw de la clase board_view. Practica MVC parte2

--- main.js
+++ main.js
@@ -1,35 +1,63 @@
-class Board{
-        constructor(width,height){
-        this.width = width;
-        this.height = height;
-        this.playing=false;
-        this.game_over=false;
-        this.bars=[];
-        this.ball=null;
-}
-        
-        get elements(){
-            let elements = this.bars;
-            elements.push(ball);
-            return elements;
-        }
+import {Board} from "./Board.js"
+
+class Bar{
+    constructor(x,y,width,height,board){
+        this.x=x;
+        this.y=y;
+        this.width=width;
+        this.height=height;
+        this.board=board;
+        this.board.bars.push(this)
+        this.kind="rectangle";
     }
+    /* self.Bar.prototype={
+        down: function(){
+
+        }
+        up: function(){
+
+        }
+
+    } */
+}
 
 
-(function(){
-    self.BoardView=function(canvas,board){
+class BoardView{
+    constructor(canvas,board){
         this.canvas = canvas;
         this.canvas.width =board.width;
         this.canvas.height=board.height;
         this.board=board;
         this.ctx=canvas.getContext("2d");
     }
-})();
+    
+    draw = function(){
+        
+        for(let i= this.board.elements.length-1;i>=0;i--) {
+                let el=this.board.elements[i];
+                draw2(this.ctx,el);
+            }
+    }
+        
+}
+function draw2(ctx,element){
+        if(element !== null ){
+            switch(element.kind){
+                case "rectangle":
+                    ctx.fillRect(element.x,element.y,element.width,element.height);
+                    break;
+            }
+        } 
+    }
 
 window.addEventListener("load",main)
 
 function main(){
     let board = new Board(800,400);
+    let bar = new Bar(20,100,40,100,board)
+    let bar2 = new Bar(700,100,40,100,board)
     let canvas = document.getElementById("canvas");
     let board_view=new BoardView(canvas,board);
+    console.log(board)
+    board_view.draw();
 }
